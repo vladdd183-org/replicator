7b66eed5a57ad819bd66991aac6448dcbbeea95f
vladdd183 <vladdd183@gmail.com>
2026-04-13 22:10:50 +0300

    fix: удалить артефакты тестовых запусков pipeline

--- src/Ship/Core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core utilities for the Ship framework."""
-
-from src.Ship.Core.retry_async import retry_async, retry_async_result
-
-__all__ = [
-    "retry_async",
-    "retry_async_result",
-]
